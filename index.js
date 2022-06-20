08e7f71435aaa51e7d305bd6509e3eff37179893
RageJustinBieber <91418494+RageJustinBieber@users.noreply.github.com>
2022-06-20 18:48:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,61 +3,61 @@ const moment = require('moment');
 const chalk = require('chalk');
 const rs = require('readline-sync');
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 const GoStumble = (auth) => new Promise((resolve, reject) => {
 
-  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
-    method: 'GET',
-    headers: {
-      'authorization': auth
-    }
-  })
+    fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+        method: 'GET',
+        headers: {
+            'authorization': auth
+        }
+    })
     .then(res => res.text())
-    .then(data => {
-      resolve(data);
+    .then(data=> {
+        resolve(data);
     })
     .catch(err => {
-      reject(err);
+        reject(err);
     });
 
 });
 
 (async () => {
 
-  console.log(`Trophy And Crown Hack Safe! [ReEdit]
-By : ${chalk.bold('Rexxy')} - Credit : @Rexxy
+    console.log(`
+ＳＴＵＭＢＬＥ   ＨＡＣＫ 
+By : ${chalk.blue('Yinyan#1581')}  ${chalk.blue('')}
 `);
 
-  const auth = rs.question('Enter Authentication Code! : ');
-  console.log('');
+    const auth = rs.question('[+] Auth Token : ');
+    console.log('');
 
-  while (true) {
+    while (true) {
 
-    const result = await GoStumble(auth);
-    if (!result) {
+        const result = await GoStumble(auth);
+        if (!result) {
 
-      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Authentication Code Not Valid`));
-      break;
+            console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] [401] Wrong Token or Expired Token !`));
+            break;
 
-    } else if (result.includes('User')) {
+        } else if (result.includes('User')) {
 
-      const data = JSON.parse(result);
-      const username = data.User.Username;
-      const country = data.User.Country;
-      const trophy = data.User.SkillRating;
-      const crown = data.User.Crowns;
-
-console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
+            const data = JSON.parse(result);
+            const username = data.User.Username;
+            const trophy = data.User.SkillRating;
+            const crown = data.User.Crowns
+            console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
       await sleep(5000);
 
-    } else if (result == 'BANNED') {
-      console.log(chalk.bgRed(`Your Account has been Banned`));
-     break;
-    }
-  }
+        } else if (result == 'BANNED') {
+            console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Akun Kamu Telah Di Banned `));
+            break;
+            
+            } else if (result == 'SERVER_ERROR') {
 
+                 continue;
+                 
+        }
+    }
+    
 
 })();
